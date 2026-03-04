110a8ab8d203dce5bb50448c9e981682b362438e
Thilo Klein <1876440+thiloklein@users.noreply.github.com>
2026-03-04 14:37:34 +0100

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -12,8 +12,15 @@ cp('-R', 'web/*', 'web_deploy/');
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
-var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
-rm('-rf', 'web_deploy/swagger-ui/')
-cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
-sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
+var SWAGGER_UI_DIST = require('swagger-ui-dist').getAbsoluteFSPath();
+
+rm('-rf', 'web_deploy/swagger-ui/');
+cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/');
+
+// In swagger-initializer.js steht standardmäßig die Petstore-URL
+sed('-i',
+  'https://petstore.swagger.io/v2/swagger.json',
+  '../swagger.json',
+  'web_deploy/swagger-ui/swagger-initializer.js'
+);
 
